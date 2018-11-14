f90cf95c394c577d8319ba71db8082fb5754df49
DeAndre Boston <dboston3@yahoo.com>
2018-11-14 12:01:44 -0600

    Deleted filter method

--- src/components/shelf.js
+++ src/components/shelf.js
@@ -10,7 +10,7 @@ class Shelf extends React.Component {
         <h2 className="bookshelf-title">{this.props.title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-          {this.props.books.filter(book => book.shelf === 'currentlyReading').map( book => (
+          {this.props.books.map( book => (
             <li key={book.id}>
               <Book
                 book={book}
