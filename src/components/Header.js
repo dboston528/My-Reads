95db52fec90afca9ba9238ff1d6f46241aa157ea
DeAndre Boston <dboston3@yahoo.com>
2019-10-15 15:05:14 -0500

    Change header to function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div className="list-books-title">
-        <h1>MyReads</h1>
-      </div>
-  );
-  }
+const Header = () => {
+  return (
+    <div className="list-books-title">
+      <h1>My Reads</h1>
+    </div>
+  )
 }
 
-export default Header;
+export default Header
+
