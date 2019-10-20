31414357b9f2afa197bd846c8d02c38216ce4b44
DeAndre Boston <dboston3@yahoo.com>
2019-10-19 21:52:34 -0500

    Add bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Nav, NavItem, Navbar } from 'react-bootstrap';
+import '../App.css';
+
 const Header = () => {
   return (
-    <Navbar bg="dark" expand="lg">
-      <Nav.Link href="deandreboston.com">My Site</Nav.Link>
-      <NavItem>About</NavItem>
+    <Navbar className="navbar" expand="lg">
+      <Navbar.Brand>MyReads</Navbar.Brand>
+      <Nav.Link href="/">My Site</Nav.Link>
+      <Nav.Link href="/about">About</Nav.Link>
     </Navbar>
   );
 };
