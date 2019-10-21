af4c62150549ff59b1a0703859cc62376fffc9c2
DeAndre Boston <dboston3@yahoo.com>
2019-10-21 00:17:29 -0500

    Add about route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
-import './App.css'
+import React from 'react';
+import { Route } from 'react-router-dom';
+import './App.css';
 import Shelves from './components/Shelves';
 import Search from './components/Search';
 import SearchButton from './components/SearchButton';
 import Header from './components/Header';
-import * as BooksAPI from "./BooksAPI";
-
+import * as BooksAPI from './BooksAPI';
+import About from './components/About';
 
 class BooksApp extends React.Component {
   state = {
@@ -18,64 +18,67 @@ class BooksApp extends React.Component {
      */
     showSearchPage: false,
     books: [],
-    query: ""
-
-  }
+    query: ''
+  };
 
   updateSearch = state => {
-    console.log("here", state)
-    this.setState({ showSearchPage: state })
-  }
+    console.log('here', state);
+    this.setState({ showSearchPage: state });
+  };
 
   componentDidMount() {
-    BooksAPI.getAll().then((resp) => this.setState({ books: resp }));
+    BooksAPI.getAll().then(resp => this.setState({ books: resp }));
   }
 
   changeBookShelf = (book, shelf) => {
-    BooksAPI
-      .update(book, shelf)
-      .then(response => {
-        let newList = this
-          .state
-          .books
-          .slice(0);
+    BooksAPI.update(book, shelf).then(response => {
+      let newList = this.state.books.slice(0);
 
-        const books = newList.filter(listBook => listBook.id === book.id);
-        if (books.length) {
-          books[0].shelf = shelf;
-        } else {
-          newList.push(book);
-          book.shelf = shelf;
-        }
-        this.setState({ books: newList });
-      })
+      const books = newList.filter(listBook => listBook.id === book.id);
+      if (books.length) {
+        books[0].shelf = shelf;
+      } else {
+        newList.push(book);
+        book.shelf = shelf;
+      }
+      this.setState({ books: newList });
+    });
   };
 
   render() {
     return (
       <div className="app">
-        <Route path="/search" render={() => (
-          <Search showSearchPage={this.updateSearch} books={this.state.books} changeShelf={this.changeBookShelf} />
-        )} />
+        <Route path="/about" render={() => <About />} />
+        <Route
+          path="/search"
+          render={() => (
+            <Search
+              showSearchPage={this.updateSearch}
+              books={this.state.books}
+              changeShelf={this.changeBookShelf}
+            />
+          )}
+        />
 
-        <Route exact path="/" render={() => (
-          <div className="list-books">
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <div className="list-books">
+              <Header />
 
+              <Shelves
+                allBooks={this.state.books}
+                changeShelf={this.changeBookShelf}
+              />
 
-            <Header />
-
-            <Shelves allBooks={this.state.books} changeShelf={this.changeBookShelf} />
-
-
-            <SearchButton showSearchPage={this.updateSearch} />
-          </div>
-
-        )} />
-
-
+              <SearchButton showSearchPage={this.updateSearch} />
+            </div>
+          )}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default BooksApp
+export default BooksApp;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, NavItem, Navbar } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 import '../App.css';
 
 const Header = () => {
