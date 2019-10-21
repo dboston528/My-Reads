e80dd41ac3e0b56a32f37e8be0772e697c740fcc
DeAndre Boston <dboston3@yahoo.com>
2019-10-21 13:38:34 -0500

    Make logo color white

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,18 @@ import '../App.css';
 
 const Header = () => {
   return (
-    <Navbar bg="dark" className="navbar" expand="lg">
+    <Navbar
+      bg="dark"
+      variant="dark"
+      className="navbar"
+      expand="lg"
+      sticky="top"
+    >
       <Navbar.Brand href="/" className="logo-text">
         MyReads
       </Navbar.Brand>
 
-      <Nav.Link id="mysite" href="/" className="navl">
+      <Nav.Link id="mysite" href="www.deandreboston.com" className="navl">
         Portfolio
       </Nav.Link>
       <Nav.Link href="/about">About</Nav.Link>
